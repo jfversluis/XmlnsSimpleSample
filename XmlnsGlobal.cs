6946eb7759879cee6bb057c92bba28b7efceff54
Gerald Versluis <gerald.versluis@microsoft.com>
2025-05-21 17:29:47 -0700

    Apply xmlns magic

---
+++ XmlnsGlobal.cs
@@ -0,0 +1,15 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Fonts")]
+
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "XmlnsSimple")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "XmlnsSimple.Pages")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "XmlnsSimple.Pages.Controls")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "XmlnsSimple.PageModels")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "XmlnsSimple.Models")]
+
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Syncfusion.Maui.Toolkit.Shimmer")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Syncfusion.Maui.Toolkit.PullToRefresh")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Syncfusion.Maui.Toolkit.TextInputLayout")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Syncfusion.Maui.Toolkit.SegmentedControl")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "http://schemas.microsoft.com/dotnet/2022/maui/toolkit")]
+
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "http://schemas.syncfusion.com/maui/toolkit")]
