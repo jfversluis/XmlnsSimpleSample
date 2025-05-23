55c7da5e8327f52f7eb812eb44c98477e8cfadc7
Gerald Versluis <gerald.versluis@microsoft.com>
2025-05-23 12:26:56 -0700

    Update and rename XmlnsGlobal.cs to GlobalXmlns.cs

--- GlobalXmlns.cs
+++ GlobalXmlns.cs
@@ -6,10 +6,6 @@
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "XmlnsSimple.PageModels")]
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "XmlnsSimple.Models")]
 
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Syncfusion.Maui.Toolkit.Shimmer")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Syncfusion.Maui.Toolkit.PullToRefresh")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Syncfusion.Maui.Toolkit.TextInputLayout")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Syncfusion.Maui.Toolkit.SegmentedControl")]
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "http://schemas.microsoft.com/dotnet/2022/maui/toolkit")]
 
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "http://schemas.syncfusion.com/maui/toolkit")]
